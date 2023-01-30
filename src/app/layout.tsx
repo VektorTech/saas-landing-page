51f5c009391fcff4be6ec231eac4ed8a436ffb56
VektorTech <kennymax18@gmail.com>
2023-01-30 02:02:45 -0500

    setup initial theming

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,24 @@
 import './globals.css'
 
-interface IProps {
-  children: React.ReactNode
-}
+import { Manrope } from "@next/font/google";
+
+const manrope = Manrope({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-manrope"
+});
 
 export default function RootLayout({
   children,
 }: IProps) {
   return (
-    <html lang="en">
+    <html lang="en" className={manrope.variable}>
       <head />
       <body>{children}</body>
     </html>
   )
+}
+
+interface IProps {
+  children: React.ReactNode
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -6,7 +8,72 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      midnight: "#121313",
+      primary: {
+        DEFAULT: "#3734A9",
+        bland: "#4F46BA",
+        dark: "#1B1C31"
+      },
+      accent: {
+        DEFAULT: "#FF7F5",
+        bland: "#F9896B"
+      },
+      neutral: {
+        100: "#FAFBFF",
+        200: "#C4C4C4",
+        300: "#EBECE8",
+        700: "#404444",
+      },
+    },
+    fontFamily: {
+      sans: ["var(--font-manrope)", ...defaultTheme.fontFamily.sans]
+    },
+    fontSize: {
+      sm: "1.142rem",
+      base: ["1rem", {
+        lineHeight: "1.625rem",
+        letterSpacing: "-2%",
+        fontWeight: "500"
+      }],
+      lg: ["1.125rem", {
+        lineHeight: "1.625rem",
+        letterSpacing: "-2%",
+        fontWeight: "500"
+      }],
+      xl: ["1.25rem", {
+        lineHeight: "1.875rem",
+        letterSpacing: "-2%",
+        fontWeight: "600"
+      }],
+      "2xl": ["1.375rem"],
+      "5xl": ["3.125rem", {
+        lineHeight: "3.5rem",
+        letterSpacing: "-3%",
+        fontWeight: "700"
+      }],
+      "6xl": ["3.75rem", {
+        fontWeight: "800"
+      }]
+    },
+    extend: {
+      colors: {
+        green: {
+          DEFAULT: "#22D497"
+        },
+        yellow: {
+          350: "#F7FA62"
+        },
+        blue: {
+          250: "#A0B3E9"
+        },
+      }
+    }
   },
   plugins: [],
 }
