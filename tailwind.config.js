21fe214d3d5a433aa7df788288c510f8807ea6d1
VektorTech <kennymax18@gmail.com>
2023-01-31 16:32:38 -0500

    modify font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -63,7 +63,11 @@ module.exports = {
         letterSpacing: "0rem",
         fontWeight: "400"
       }],
-      "2xl": ["1.375rem"],
+      "2xl": ["1.375rem", {
+        lineHeight: "1.875rem",
+        letterSpacing: "-0.025rem",
+        fontWeight: "600"
+      }],
       "5xl": ["3.125rem", {
         lineHeight: "3.5rem",
         letterSpacing: "-0.025rem",
