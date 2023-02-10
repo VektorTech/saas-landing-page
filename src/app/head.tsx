9ef84b87b7791e9837d1b47f3837cac14572d2fc
VektorTech <kennymax18@gmail.com>
2023-02-09 23:28:47 -0500

    add meta tags & description

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,10 +1,10 @@
 export default function Head() {
   return (
     <>
-      <title></title>
+      <title>Crypto Fin | Manage Crypto Portfolio</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="" />
-      <link rel="icon" href="/favicon.ico" />
+      <meta name="description" content="End-to-end payments and financial management in a single solution. Meet the right platform to help realize." />
+      <link rel="icon" href="/favicon.ico?v=1" />
     </>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,42 @@ import GDPRFeature from "@/components/GDPRFeature";
 import ValuePropositions from "@/components/ValuePropositions";
 import Blog from "@/components/Blog";
 
+const title = "Crypto Fin | Manage Crypto Portfolio";
+const description =
+  "End-to-end payments and financial management in a single solution. Meet the right platform to help realize.";
+const author = "Kenny Sutherland";
+const keywords =
+  "Saas, startup, next.js, react, crypto, wallet, bitcoin, blockchain, landing-page, fin-tech, finance".split(
+    ", "
+  );
+const url = "https://crypto-finance.vercel.app";
+
+export const metadata = {
+  title,
+  description,
+  keywords,
+  author,
+  themeColor: "#3734A9",
+  openGraph: {
+    title,
+    description,
+    url,
+    siteName: "Crypto Fin",
+    locale: "en-US",
+    type: "website",
+  },
+  twitter: {
+    title,
+    description,
+  },
+  alternates: {
+    canonical: url,
+    languages: {
+      "en-US": url,
+    },
+  },
+};
+
 export default function Home() {
   return (
     <>
