d755f8e12e75533346278748edaa868583d1397d
VektorTech <kennymax18@gmail.com>
2023-02-11 03:17:27 -0500

    remove anim

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -109,24 +109,18 @@ export default function Hero() {
           </div>
         </div>
         <article className="container text-center pt-28 md:pt-32 xl:pt-40">
-          <h2 className="text-white !font-bold text-3xl sm:text-4xl lg:text-6xl animate-slide-intro">
+          <h2 className="text-white !font-bold text-3xl sm:text-4xl lg:text-6xl">
             Managing your crypto portfolio
             <br />
             has never been easier
           </h2>
-          <p
-            className="text-white text-sm md:text-base lg:text-xl !font-light lg:tracking-[0] mt-8 px-10 lg:px-0 animate-slide-intro"
-            style={{ animationDelay: "0.05s" }}
-          >
+          <p className="text-white text-sm md:text-base lg:text-xl !font-light lg:tracking-[0] mt-8 px-10 lg:px-0">
             End-to-end payments and financial management in a single solution.
             Meet
             <br />
             the right platform to help realize.
           </p>
-          <div
-            className="mt-10 md:mt-14 flex gap-3 sm:gap-8 md:gap-12 justify-center whitespace-nowrap origin-top scale-75 md:scale-100 animate-slide-intro"
-            style={{ animationDelay: "0.1s" }}
-          >
+          <div className="mt-10 md:mt-14 flex gap-3 sm:gap-8 md:gap-12 justify-center whitespace-nowrap origin-top scale-75 md:scale-100">
             <Button href="#" bgColor="orange" rounded="rounded-full">
               Get Started
             </Button>
