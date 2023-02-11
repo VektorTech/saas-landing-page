7955fc16b6df6ede83722e2371a478976e843587
VektorTech <kennymax18@gmail.com>
2023-02-11 01:28:51 -0500

    fix bug in banner animation calc.

--- src/components/BannerImage.tsx
+++ src/components/BannerImage.tsx
@@ -9,16 +9,19 @@ const BannerImage = () => {
       <div
         onMouseMove={(event) => {
           if (event.target instanceof HTMLDivElement) {
+            const scrollTop = document.documentElement.scrollTop;
+            const scrollLeft = document.documentElement.scrollLeft;
             const { left, top, width, height } =
               event.target.getBoundingClientRect();
-            const midX = left + width / 2;
-            const midY = top + height / 2;
+            const halfWidth = width / 2;
+            const halfHeight = height / 2;
+            const midX = scrollLeft + left + halfWidth;
+            const midY = scrollTop + top + halfHeight;
 
-            const x = (event.clientX - midX) / midX;
-            const y = (midY - event.clientY) / midY;
-
-            event.target.style.setProperty("--x-rot", `${x * 8}`);
-            event.target.style.setProperty("--y-rot", `${y * 8}`);
+            const x = (scrollLeft + event.clientX - midX) / halfWidth;
+            const y = (midY - (scrollTop + event.clientY)) / halfHeight;
+            event.target.style.setProperty("--x-rot", `${x * 6}`);
+            event.target.style.setProperty("--y-rot", `${y * 6}`);
           }
         }}
         onMouseLeave={(event) => {
@@ -39,21 +42,21 @@ const BannerImage = () => {
         >
           <div
             style={{
-              transform: `rotateX(calc(var(--y-rot, 0)*1deg)) rotateY(calc(var(--x-rot, 0)*1deg)) translateZ(-1rem)`,
+              transform: `rotateX(calc(var(--y-rot, 0)*1deg)) rotateY(calc(var(--x-rot, 0)*1deg)) translateZ(-0.6rem)`,
               transition: "var(--transition)",
             }}
             className="bg-[#ffffff16] rounded-lg absolute w-[calc(100%_+_6rem)] h-[calc(83%_+_6rem)] -ml-[3rem] -mt-[3rem]"
           ></div>
           <div
             style={{
-              transform: `rotateX(calc(var(--y-rot, 0)*1deg)) rotateY(calc(var(--x-rot, 0)*1deg)) translateZ(-0.8rem)`,
+              transform: `rotateX(calc(var(--y-rot, 0)*1deg)) rotateY(calc(var(--x-rot, 0)*1deg)) translateZ(-0.4rem)`,
               transition: "var(--transition)",
             }}
             className="bg-[#ffffff16] rounded-lg absolute w-[calc(100%_+_4rem)] h-[calc(83%_+_4rem)] -ml-[2rem] -mt-[2rem]"
           ></div>
           <div
             style={{
-              transform: `rotateX(calc(var(--y-rot, 0)*1deg)) rotateY(calc(var(--x-rot, 0)*1deg)) translateZ(-0.6rem)`,
+              transform: `rotateX(calc(var(--y-rot, 0)*1deg)) rotateY(calc(var(--x-rot, 0)*1deg)) translateZ(-0.2rem)`,
               transition: "var(--transition)",
             }}
             className="bg-[#ffffff16] rounded-lg absolute w-[calc(100%_+_2rem)] h-[calc(83%_+_2rem)] -ml-[1rem] -mt-[1rem]"
