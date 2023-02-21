b7c9259268d51f1883a11eebc995123099c7d6b5
VektorTech <kennymax18@gmail.com>
2023-02-21 04:15:18 -0500

    change blog section layout at each breakpoint

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -7,39 +7,41 @@ import StockImage2 from "@/images/blog-stock-2.jpg";
 import StockImage3 from "@/images/blog-stock-3.jpg";
 import StockImage4 from "@/images/blog-stock-4.jpg";
 
+// mt-7 space-y-7 lg:space-y-0
+
 export default function Blog() {
   return (
     <section className="container mt-28">
       <h2 className="text-3xl md:text-5xl font-extrabold">Recent Blogs</h2>
 
-      <Link href="/">
-        <div className="bg-white flex flex-col lg:flex-row mt-10 md:mt-20">
-          <article className="pt-4 md:pt-6 px-3 md:px-8 pb-10 lg:w-3/5">
-            <p className="uppercase text-sm md:text-base !font-extrabold">
-              best practices
-            </p>
-            <h3 className="text-lg sm:text-xl md:text-blog-heading !font-extrabold !tracking-tight mt-3 clamp-line-3 max-w-[80%]">
-              In design active temper be uneasy. Thirty for remove plenty
-              regard.
-            </h3>
-            <p className="mt-16 text-base font-extrabold">
-              Read More <RightArrow className="inline ml-2" />
-            </p>
-          </article>
-          <Image
-            alt="blog stock image 1"
-            className="w-full lg:w-[40%] object-cover"
-            src={StockImage1}
-            width={Math.round(StockImage1.width * 0.94)}
-            height={Math.round(StockImage1.height * 0.94)}
-          />
-        </div>
-      </Link>
+      <div className="grid items-start lg:grid-cols-3 md:grid-cols-2 md:grid-rows-2 lg:grid-rows-none lg:items-stretch gap-x-7 gap-y-7 mt-10 md:mt-20">
+        <Link href="/" className="lg:col-span-3">
+          <div className="bg-white h-full flex flex-col lg:flex-row">
+            <article className="pt-4 md:pt-6 px-3 md:px-8 pb-10 lg:w-3/5 md:h-[18.75rem] lg:h-auto">
+              <p className="uppercase text-sm md:text-base !font-extrabold">
+                best practices
+              </p>
+              <h3 className="text-lg sm:text-xl md:text-blog-heading !font-extrabold !tracking-tight mt-3 clamp-line-3 max-w-[80%]">
+                In design active temper be uneasy. Thirty for remove plenty
+                regard.
+              </h3>
+              <p className="mt-16 text-base font-extrabold">
+                Read More <RightArrow className="inline ml-2" />
+              </p>
+            </article>
+            <Image
+              alt="blog stock image 1"
+              className="w-full lg:w-[40%] object-cover"
+              src={StockImage1}
+              width={Math.round(StockImage1.width * 0.94)}
+              height={Math.round(StockImage1.height * 0.94)}
+            />
+          </div>
+        </Link>
 
-      <div className="grid lg:grid-cols-3 gap-x-7 space-y-7 lg:space-y-0 mt-7">
         <Link href="/">
-          <div className="bg-white">
-            <article className="pt-4 md:pt-6 px-3 md:px-8 h-56 overflow-hidden">
+          <div className="bg-white h-full flex flex-col justify-between">
+            <article className="pt-4 md:pt-6 px-3 md:px-8 pb-10 overflow-hidden md:h-[18.75rem] lg:h-auto">
               <p className="uppercase text-sm md:text-base !font-extrabold">
                 best practices
               </p>
@@ -58,8 +60,8 @@ export default function Blog() {
         </Link>
 
         <Link href="/">
-          <div className="bg-white">
-            <article className="pt-4 md:pt-6 px-3 md:px-8 h-56 overflow-hidden">
+          <div className="bg-white h-full flex flex-col justify-between">
+            <article className="pt-4 md:pt-6 px-3 md:px-8 pb-10 overflow-hidden md:h-[18.75rem] lg:h-auto">
               <p className="uppercase text-sm md:text-base !font-extrabold">
                 best practices
               </p>
@@ -78,8 +80,8 @@ export default function Blog() {
         </Link>
 
         <Link href="/" className="">
-          <div className="bg-white">
-            <article className="pt-4 md:pt-6 px-3 md:px-8 h-56 overflow-hidden">
+          <div className="bg-white h-full flex flex-col justify-between">
+            <article className="pt-4 md:pt-6 px-3 md:px-8 pb-10 overflow-hidden md:h-[18.75rem] lg:h-auto">
               <p className="uppercase text-sm md:text-base !font-extrabold">
                 best practices
               </p>
